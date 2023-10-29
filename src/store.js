fce08527442a89b9c90e4755e92f8ddb77cc6678
Ali Habibi <alihabibiwork2000@gmail.com>
2023-10-29 22:48:30 +0330

    create initial state file

--- src/store.js
+++ src/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../src/features/counter/counterSlice"
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        counter : counterReducer
+    },
 })
